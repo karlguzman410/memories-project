23c12d0e59e25a9daa897577f0e68f275341d40e
Karl Guzman <guzmankarl0410@gmail.com>
2021-01-29 14:54:56 -0800

    getPosts and createPosts added

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,25 @@
 //handler for our routes
+import PostMessage from '../models/postMessage.js'
 
-export const getPosts = (request, response) => {
-    response.send('getPosts() - works')
+export const getPosts = async (request, response) => {
+    try {
+        const postMessages = await PostMessage.find()
+        console.log(postMessages)
+        response.status(200).json(postMessages)
+    } catch (error) {
+        response.status(404).json({ message: error.message })
+    }
+}
+
+//https://restapitutorial.com/httpstatuscodes.html
+
+export const createPost = async (request, response) => {
+    const post = request.body
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save()
+        response.status(201).json(newPost)
+    } catch (error) {
+        response.status(409).json({ message: error.message })
+    }
 }
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getPosts } from '../controllers/posts.js'
+import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 //Path, call back function to be executed when someone visits this route
 //reached at localhost:5000/posts
 router.get('/', getPosts)
+router.post('/', createPost)
 
 
 export default router

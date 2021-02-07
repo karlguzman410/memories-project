7242d608ae4ba385c2aafd6a6b3dc71696777da8
Karl Guzman <guzmankarl0410@gmail.com>
2021-02-07 12:47:06 -0800

    Signup and signin UI added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import memories from './images/memories.png'
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import useStyles from './styles'
-import { useDispatch } from 'react-redux'
-import { getPosts } from './actions/posts'
+import React from 'react'
+import { Container } from '@material-ui/core'
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Auth from './components/Auth/Auth'
+
 
 
 const App = () => {
-    const classes = useStyles()
-
-    const [currentId, setCurrentId] = useState(null)
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch, currentId])
-
     return (
-        <Container maxWidth="lg">
-            <AppBar position="static" color="inherit" className={classes.appBar}>
-                <Typography variant="h2" className={classes.heading}>Memories</Typography>
-                <img src={memories} alt="memories" className={classes.image} />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+
     )
 }
 
--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -2,29 +2,9 @@ import { makeStyles } from '@material-ui/core/styles'
 import shadows from '@material-ui/core/styles/shadows'
 
 export default makeStyles((theme) => ({
-    appBar: {
-        borderRadius: 15,
-        margin: '30px 0',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(178, 192, 180, 0.5)'
-    },
-    heading: {
-        color: 'rgba(126, 184, 135, 1)',
-    },
-    image: {
-        marginLeft: '15px',
-        height: "100px",
-    },
     [theme.breakpoints.down('sm')]: {
         mainContainer: {
             flexDirection: "column-reverse"
-        },
-        heading: {
-            color: 'black',
-            fontSize: '2.0rem'
         }
     },
 
